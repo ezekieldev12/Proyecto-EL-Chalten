e6213f6396874677442bd33a3f2d2c023e4916a8
Ezequiel Gauna <ezequielgaunaa@gmail.com>
2022-10-08 18:10:27 -0300

    chalten

---
+++ app.js
@@ -0,0 +1,46 @@
+//variables booleanas verdadero TRUE o falso FALSE
+//IF = SI (si pasa esto ejecutar cierta porcion de codigo)
+
+
+//ciclos bucles de JAVASCRIPT= for while DoWhile
+
+//for= porque quiero repetir una instruccion de codigo una cantidad especifica de veces.
+
+//continue= es cuando queremos que el for se repita varias veces pero no queremos ejecutar el codigo que tiene dentro.
+
+//switch= condiciones de maneras mas compactas.
+
+//estructura for:
+
+/* for(desde ; hasta ; actualización ) {
+    lo que se escriba aca se ejecutara mientras que dure el ciclo
+}
+*/
+//desde: es la zona que se establecen los valores iniciales de las variables que controlan el ciclo
+
+//EJEMPLO FLOR PARA CONTAR HASTA 9
+
+/*
+for (let i = 0; i < 10; i++) {
+
+}
+*/
+
+
+
+/*let nombre = prompt("Ingresa tu nombre");
+let clave = "Micaela" ;
+if (nombre == clave){
+    alert(nombre +", " + "el hombre que tanto esperas es Ezequiel Gauna. No seas boluda y dale bola");
+}else {
+    alert("...");
+}*/
+
+
+
+
+
+
+
+
+
